9bcef91e14dc2dc66175c8ec4cf89612463500e0
Tent <pawan1235@hotmail.co.th>
2017-04-03 15:03:34 +0700

    add sumDigit

--- Rec.java
+++ Rec.java
@@ -17,7 +17,10 @@ public class Rec {
       Precondition: n >= 0. */
     public static int sumDigits(int n) {
 	
-	// fill me in
+	if (n / 10 == 0) {
+			return n;
+		} else
+			return n % 10 + sumDigits(n / 10);
 	
     }
 	
